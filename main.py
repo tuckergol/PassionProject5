8f660eb2c00ca8231867847e98ad4b269c9354ce
wl0503 <seungs0503@gmail.com>
2023-11-02 18:00:14 -0700

    main.py change according to geoguesser

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.players import initPlayers
+from model.model_geoguesser import initGeoguesser
 
 
 # setup APIs
@@ -15,6 +16,7 @@ from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
+from api.geoguesser import geoguesser_api
 
 
 # setup App pages
@@ -30,6 +32,7 @@ app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(geoguesser_api)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -49,6 +52,7 @@ def activate_job():  # activate these items
     initJokes()
     initUsers()
     initPlayers()
+    initGeoguesser()
 
 # this runs the application on the development server
 if __name__ == "__main__":
